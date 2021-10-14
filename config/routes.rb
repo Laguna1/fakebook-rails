39ca52a2073264beb60eee160af2a508740e15d4
Laguna1 <lagunazoomarket@gmail.com>
2021-10-14 21:54:43 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
  root 'users#index'
   devise_for :users
   resources :users, only: %i[index show] do
-    resources :friendships, only: :create
+    resources :friendships, only: :create do
+      collection do
+        get 'accept_friend'
+        get 'decline_friend'
+      end
+    end
   end
   resources :posts, only: %i[index show new create destroy] do
     resources :likes, only: :create
